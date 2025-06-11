0b31e4103a73c56c260f50dca8564d1fbafd546e
Elad Bar <elad.bar@hotmail.com>
2025-06-11 10:21:44 +0300

    update cursor rule and add usage.md

--- mcp_alchemy_with_vertica/server.py
+++ mcp_alchemy_with_vertica/server.py
@@ -58,7 +58,7 @@ def get_db_info():
 
 ### Constants ###
 
-VERSION = "2025.6.11.094926"
+VERSION = "2025.6.11.101758"
 DB_INFO = get_db_info()
 EXECUTE_QUERY_MAX_CHARS = int(os.environ.get('EXECUTE_QUERY_MAX_CHARS', 4000))
 CLAUDE_LOCAL_FILES_PATH = os.environ.get('CLAUDE_LOCAL_FILES_PATH')
